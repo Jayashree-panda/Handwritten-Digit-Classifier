276012c13b08f7882d6a5efdb5981ea5a17e703f
Jayashree-panda <jayashreepanda12345@gmail.com>
2020-05-13 08:21:40 +0530

    prepare pixel data

--- handDigitClass.py
+++ handDigitClass.py
@@ -1,24 +1,7 @@
 from keras.datasets import mnist
 from matplotlib import pyplot
 from keras import utils as np_utils
-# load dataset
 
-
-# summarize loaded dataset
-'''print('Train: X=%s, y=%s' % (trainX.shape, trainY.shape))
-print('Test: X=%s, y=%s' % (testX.shape, testY.shape))'''
-
-# plot first few images
-'''for i in range(9):
-	# define subplot
-	pyplot.subplot(330 + 1 + i)
-	# plot raw pixel data
-	pyplot.imshow(trainX[i], cmap=pyplot.get_cmap('gray'))
-# show the figure
-pyplot.show()'''
-
-	
-# load train and test dataset
 def load_dataset():
 	# load dataset
 	(trainX, trainY), (testX, testY) = mnist.load_data()
@@ -29,5 +12,17 @@ def load_dataset():
 	trainY = np_utils.to_categorical(trainY)
 	testY = np_utils.to_categorical(testY)
 	return trainX, trainY, testX, testY
+	
+# scale pixels
+def prep_pixels(train, test):
+	# convert from integers to floats
+	train_norm = train.astype('float32')
+	test_norm = test.astype('float32')
+	# normalize to range 0-1
+	train_norm = train_norm / 255.0
+	test_norm = test_norm / 255.0
+	# return normalized images
+	return train_norm, test_norm
 
-load_dataset()
+trainX, trainY, testX, testY = load_dataset()
+prep_pixels(trainX, testX)

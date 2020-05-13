726ae2071bdd56ea2971591c3ab2952b8e71c280
Jayashree-panda <jayashreepanda12345@gmail.com>
2020-05-13 09:36:31 +0530

    CNN model

--- handDigitClass.py
+++ handDigitClass.py
@@ -1,6 +1,12 @@
 from keras.datasets import mnist
 from matplotlib import pyplot
 from keras import utils as np_utils
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+from keras.optimizers import SGD
 
 def load_dataset():
 	# load dataset
@@ -12,6 +18,8 @@ def load_dataset():
 	trainY = np_utils.to_categorical(trainY)
 	testY = np_utils.to_categorical(testY)
 	return trainX, trainY, testX, testY
+
+trainX, trainY, testX, testY = load_dataset()
 	
 # scale pixels
 def prep_pixels(train, test):
@@ -23,6 +31,22 @@ def prep_pixels(train, test):
 	test_norm = test_norm / 255.0
 	# return normalized images
 	return train_norm, test_norm
-
-trainX, trainY, testX, testY = load_dataset()
+	
 prep_pixels(trainX, testX)
+
+# define cnn model
+def define_model():
+	model = Sequential()
+	model.add(Convolution2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(28, 28, 1)))
+	model.add(MaxPooling2D((2, 2)))
+	model.add(Flatten())
+	model.add(Dense(100, activation='relu', kernel_initializer='he_uniform'))
+	model.add(Dense(10, activation='softmax'))
+	# compile model
+	opt = SGD(lr=0.01, momentum=0.9)
+	model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+	return model
+
+
+
+define_model()
